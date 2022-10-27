57794bd9161428c86973019bb4b1957beffb3216
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-10-27 14:12:31 +0200

    Refactored variables

--- main.py
+++ main.py
@@ -2,55 +2,61 @@ import resources.view_generator as vgen
 import resources.models as models
 from pynput import keyboard
 
-FramedList = models.framedlist()
-
 class CLUIEngine:
     def __init__(self, model, resolution, controlling_keys):
-        print(model)
         self.position = [0, 0]
-        self.content = []
+        self.canvas = {}
+        self.ishidden = True
+
         try:
-            self.canvas_width = int(resolution.split('x')[0])
-            self.canvas_height = int(resolution.split('x')[1])
+            self.canvas['width'] = int(resolution.split('x')[0])
+            self.canvas['height'] = int(resolution.split('x')[1])
         except:
             print('CLUIE: Canvas resolution fetching error. Check if you typed it in following format [WIDTH]x[HEIGHT].')
 
         try:
-            with open('model.cluie', 'r', encoding='utf-8') as load_model:
-                self.view_model = self.fetch_model(load_model.readlines())
+            if type(model) == str:
+                with open('model.cluie', 'r', encoding='utf-8') as load_model:
+                    self.view_model = self.fetch_model(load_model.readlines())
+            else:
+                match model[0]:
+                    case 'FramedList':
+                        self.model = models.get_model(model[0], self.canvas_width, self.canvas_height, model[1])
         except:
             print('CLUIE: Model importing error. Make sure it\'s placed in the same folder and is properly prepared.')
         
         try:
-            if controlling_keys == 'WSADE':
-                self.key_up, self.key_down, self.key_left, self.key_right, self.key_submit = 'w', 's', 'a', 'd', 'Key.enter'
-            elif controlling_keys == 'ARROWSE':
-                self.key_up, self.key_down, self.key_left, self.key_right, self.key_submit = 'Key.up', 'Key.down', 'Key.left', 'Key.right', 'Key.enter'
-            elif controlling_keys == 'WSADS':
-                self.key_up, self.key_down, self.key_left, self.key_right, self.key_submit = 'w', 's', 'a', 'd', 'Key.space'
-            elif controlling_keys == 'ARROWSS':
-                self.key_up, self.key_down, self.key_left, self.key_right, self.key_submit = 'Key.up', 'Key.down', 'Key.left', 'Key.right', 'Key.space'
-            else:
-                self.key_up = controlling_keys[0]
-                self.key_down = controlling_keys[1]
-                self.key_left = controlling_keys[2]
-                self.key_right = controlling_keys[3]
-                self.key_submit = controlling_keys[4]
+            match controlling_keys:
+                case 'WSADE':
+                    self.canvas['controllers']['']
+                    self.canvas['controllers']['up'], self.canvas['controllers']['down'], self.canvas['controllers']['left'], self.canvas['controllers']['right'], self.canvas['controllers']['submit'] = 'w', 's', 'a', 'd', 'Key.enter'
+                case 'ARROWSE':
+                    self.canvas['controllers']['up'], self.canvas['controllers']['down'], self.canvas['controllers']['left'], self.canvas['controllers']['right'], self.canvas['controllers']['submit'] = 'Key.up', 'Key.down', 'Key.left', 'Key.right', 'Key.enter'
+                case 'WSADS':
+                    self.canvas['controllers']['up'], self.canvas['controllers']['down'], self.canvas['controllers']['left'], self.canvas['controllers']['right'], self.canvas['controllers']['submit'] = 'w', 's', 'a', 'd', 'Key.space'
+                case 'ARROWSS':
+                    self.canvas['controllers']['up'], self.canvas['controllers']['down'], self.canvas['controllers']['left'], self.canvas['controllers']['right'], self.canvas['controllers']['submit'] = 'Key.up', 'Key.down', 'Key.left', 'Key.right', 'Key.space'
+                case _:
+                    self.canvas['controllers']['up'] = controlling_keys[0]
+                    self.canvas['controllers']['down'] = controlling_keys[1]
+                    self.canvas['controllers']['left'] = controlling_keys[2]
+                    self.canvas['controllers']['right'] = controlling_keys[3]
+                    self.canvas['controllers']['submit'] = controlling_keys[4]
         except:
             print('CLUIE: Controlling keys assign error. Check documentation for more information.')
 
         listener = keyboard.Listener(on_press=self.on_press).start()
     
-    def fetch_model(self, model):
-        print('asd')
+    def display(self):
+        self.ishidden = False
 
     def on_press(self, key):
         key = str(key).replace('\'', '')
         pressed_key = None
         match key:
-            case self.key_up: pressed_key = 'up'; self.position[1] += 1
-            case self.key_down: pressed_key = 'down'; self.position[1] -= 1
-            case self.key_left: pressed_key = 'left'; self.position[0] -= 1
-            case self.key_right: pressed_key = 'right'; self.position[0] += 1
-            case self.key_submit: pressed_key = 'submit'; self.position = [0, 0]
-        if pressed_key != None: vgen.update_canvas(pressed_key, self.view_model, self.position, self.content)
+            case self.canvas['controllers']['up']: pressed_key = 'up'; self.position[1] += 1
+            case self.canvas['controllers']['down']: pressed_key = 'down'; self.position[1] -= 1
+            case self.canvas['controllers']['left']: pressed_key = 'left'; self.position[0] -= 1
+            case self.canvas['controllers']['right']: pressed_key = 'right'; self.position[0] += 1
+            case self.canvas['controllers']['submit']: pressed_key = 'submit'; self.position = [0, 0]
+        if pressed_key != None and not self.ishidden: vgen.update_canvas(pressed_key, self.view_model, self.position, self.content)
