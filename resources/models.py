6cafe1b3d35960dac08f743e48564d351aeae5d3
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-10-27 15:30:07 +0200

    Improved code for testing and validating purposes

--- resources/models.py
+++ resources/models.py
@@ -1,16 +1,25 @@
+def calculate_widths(settings, new_column=None):
+    columns, raw_columns = [], []
+    for i in settings.columns:
+        raw_columns.append(i)
+    total_autos, total_manuals = 0, 0
+    if new_column != None:
+        raw_columns.append(new_column)
+    for column in range(len(raw_columns)):
+        if raw_columns[column][1] == 'auto':
+            total_autos += 1
+        else:
+            total_manuals += raw_columns[column][1]
+    for column in range(len(raw_columns)):
+        if raw_columns[column][1] == 'auto':
+            columns.append(int((settings.canvas_width-total_manuals-len(raw_columns)+1)/total_autos))
+        else:
+            columns.append(raw_columns[column][1])
+    return columns
+
 def get_model(settings, model_type):
-    if model_type == 'framedlist':
-        columns = []
-        total_autos = 0
-        total_manuals = 0
-        for column in range(len(settings.columns)):
-            if settings.columns[column][1] == 'auto':
-                total_autos += 1
-            else:
-                total_manuals += settings.columns[column][1]
-        for column in range(len(settings.columns)):
-            if settings.columns[column][1] == 'auto':
-                columns.append([settings.columns[column][0], int((settings.canvas_width-total_manuals-len(settings.columns)+1)/total_autos)])
-            else:
-                columns.append(settings.columns[column])
-        return columns
+    columns = []
+    column_sizes = calculate_widths(settings)
+    for i in range(len(settings.columns)):
+        columns.append([settings.columns[i][0], column_sizes[i]])
+    return columns
