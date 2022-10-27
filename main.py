efcfc798775de03c04735828103eb96c1254cee6
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-10-27 12:00:32 +0200

    Build class base with arguments validation

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import resources.view_generator
 from pynput import keyboard
 
-def on_press(key):
-    print(key)
+class CLUIEngine:
+    def __init__(self, resolution, controlling_keys):
+        try:
+            self.canvas_width = int(resolution.split('x')[0])
+            self.canvas_height = int(resolution.split('x')[1])
+        except:
+            print('CLUIE: Canvas resolution fetching error. Check if you typed it in following format [WIDTH]x[HEIGHT]')
 
-listener = keyboard.Listener(on_press=on_press).start()
+        listener = keyboard.Listener(on_press=CLUIEngine.on_press).start()
+    
+    def on_press(key):
+        print(key)
