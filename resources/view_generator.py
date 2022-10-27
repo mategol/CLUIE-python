5e0b2357a90dd8a5c9a00366f2a93a1ce6e3aa9e
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-10-27 23:52:24 +0200

    Fixed bugs with selected row

--- resources/view_generator.py
+++ resources/view_generator.py
@@ -3,11 +3,17 @@ from os import system
 
 init(autoreset=True)
 
-def update_canvas(reason, model, position, content, settings):
+def update_canvas(reason, model, position, content, settings, controller):
     if model['model_id'] == 'FramedList':
         ready_row = ''
         system('cls')
         print(model['first_row'] + '\n' + model['second_row'] + '\n' + model['third_row'])
+        if position[1] > len(content)-1:
+            position[1] = len(content)-1
+            controller.position[1] = len(content)-1
+        elif position[1] < 0:
+            position[1] = 0
+
         for row in range(len(content)):
             if position[1] == row:
                 ready_row = model['divider'] + Back.WHITE + Fore.BLACK
@@ -15,3 +21,4 @@ def update_canvas(reason, model, position, content, settings):
                 ready_row += model['divider'] if position[1] != row else (' ' if cell != 0 else '')
                 ready_row += content[row][cell] + ' '*(settings[cell]-len(content[row][cell]))
             print(ready_row + Back.RESET + Fore.RESET + model['divider'])
+            ready_row = ''
