02d1edb34a8f31ae1bb6fd7afa5530ee035d13a3
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-10-27 14:50:40 +0200

    Added column auto width calculation

--- resources/models.py
+++ resources/models.py
@@ -1,2 +1,16 @@
-def framedlist():
-    return 'asd'
+def get_model(settings, model_type):
+    if model_type == 'framedlist':
+        columns = []
+        total_autos = 0
+        total_manuals = 0
+        for column in range(len(settings.columns)):
+            if settings.columns[column][1] == 'auto':
+                total_autos += 1
+            else:
+                total_manuals += settings.columns[column][1]
+        for column in range(len(settings.columns)):
+            if settings.columns[column][1] == 'auto':
+                columns.append([settings.columns[column][0], int((settings.canvas_width-total_manuals)/total_autos)])
+            else:
+                columns.append(settings.columns[column])
+        return columns
