7dab68d7f38b3c3a3f066f183b4f74b840f34cc6
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-11-02 16:55:39 -0500

    Added row printing

--- resources/view_generator.py
+++ resources/view_generator.py
@@ -3,7 +3,7 @@ from os import system
 
 init(autoreset=True)
 
-def update_canvas(reason, model, position, content, settings, controller):
+def update_canvas(reason, model, position, content, settings, view_anchor, controller):
     if model['model_id'] == 'FramedList':
         ready_row = ''
         system('cls')
@@ -14,11 +14,18 @@ def update_canvas(reason, model, position, content, settings, controller):
         elif position[1] < 0:
             position[1] = 0
 
-        for row in range(len(content)):
-            if position[1] == row:
+        for row in range(view_anchor-8, view_anchor+8):
+            if row == position[1]:
                 ready_row = model['divider'] + Back.WHITE + Fore.BLACK
-            for cell in range(len(content[row])):
-                ready_row += model['divider'] if position[1] != row else (' ' if cell != 0 else '')
-                ready_row += content[row][cell] + ' '*(settings[cell]-len(content[row][cell]))
-            print(ready_row + Back.RESET + Fore.RESET + model['divider'])
-            ready_row = ''
+                for cell in range(len(content[row])):
+                    ready_row += content[row][cell] + ' '*(settings[cell]-len(content[row][cell])+1)
+                ready_row = ready_row[:-1] + Back.RESET + Fore.RESET + model['divider']
+            elif row < len(content):
+                ready_row = model['divider']
+                for cell in range(len(content[row])):
+                    ready_row += content[row][cell] + ' '*(settings[cell]-len(content[row][cell])) + model['divider']
+            else:
+                ready_row = model['divider']
+                for cell in range(len(settings)):
+                    ready_row += ' '*((settings[cell])) + model['divider']
+            print(ready_row)
