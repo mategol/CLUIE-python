30b18b31c4d50f71341bc83054c3743bacbd7b05
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-11-15 15:08:17 +0100

    Changed variables naming

--- resources/models.py
+++ resources/models.py
@@ -1,6 +1,6 @@
-def calculate_widths(settings, new_column=None):
+def calculate_widths(controller, new_column=None):
     columns, raw_columns = [], []
-    for i in settings.columns:
+    for i in controller.columns:
         raw_columns.append(i)
     total_autos, total_manuals = 0, 0
     if new_column != None:
@@ -12,19 +12,19 @@ def calculate_widths(settings, new_column=None):
             total_manuals += raw_columns[column][1]
     for column in range(len(raw_columns)):
         if raw_columns[column][1] == 'auto':
-            columns.append(int((settings.canvas_width-total_manuals-len(raw_columns)+1)/total_autos))
+            columns.append(int((controller.canvas_width-total_manuals-len(raw_columns)+1)/total_autos))
         else:
             columns.append(raw_columns[column][1])
     return columns
 
-def get_model(settings, model_type):
+def get_model(controller, model_type):
     columns = []
-    column_sizes = calculate_widths(settings)
+    column_sizes = calculate_widths(controller)
     first_row, second_row, third_row = '┏', '┃', '┣'
-    for i in range(len(settings.columns)):
-        columns.append([settings.columns[i][0], column_sizes[i]])
+    for i in range(len(controller.columns)):
+        columns.append([controller.columns[i][0], column_sizes[i]])
         first_row += '━'*columns[i][1] + '┳'
-        second_row += ((' '*settings.settings['column_label_margin'] + columns[i][0] + ' '*(columns[i][1]-len(columns[i][0])-settings.settings['column_label_margin'])) if len(columns[i][0])+settings.settings['column_label_margin'] <= columns[i][1] else (columns[i][0][:columns[i][1]-1-settings.settings['column_label_margin']] + '…')) + '┃'
+        second_row += ((' '*controller.settings['column_label_margin'] + columns[i][0] + ' '*(columns[i][1]-len(columns[i][0])-controller.settings['column_label_margin'])) if len(columns[i][0])+controller.settings['column_label_margin'] <= columns[i][1] else (columns[i][0][:columns[i][1]-1-controller.settings['column_label_margin']] + '…')) + '┃'
         third_row += '━'*columns[i][1] + '╋'
     first_row, third_row = first_row[:-1] + '┓', third_row[:-1] + '┫'
 
