3f08dc6f04dc54f674a1759872ab0e9380c69a89
Mateusz Gołembowski <44233157+mategol@users.noreply.github.com>
2022-11-16 15:45:22 +0100

    Added header feature

--- resources/models.py
+++ resources/models.py
@@ -27,5 +27,11 @@ def get_model(controller, model_type):
         second_row += ((' '*controller.settings['column_label_margin'] + columns[i][0] + ' '*(columns[i][1]-len(columns[i][0])-controller.settings['column_label_margin'])) if len(columns[i][0])+controller.settings['column_label_margin'] <= columns[i][1] else (columns[i][0][:columns[i][1]-1-controller.settings['column_label_margin']] + '…')) + '┃'
         third_row += '━'*columns[i][1] + '╋'
     first_row, third_row = first_row[:-1] + '┓', third_row[:-1] + '┫'
-
-    return {'model_id': 'FramedList', 'first_row': controller.settings['margin_top']*'\n' + first_row + controller.settings['margin_right']*' ', 'second_row': second_row + controller.settings['margin_right']*' ', 'third_row': third_row + controller.settings['margin_right']*' ', 'divider': '┃', 'columns': columns}
+    if len(controller.settings['top_header']) > 0:
+        if len(controller.settings['top_header']) > controller.settings['margin_top']:
+            header = '\n'.join(controller.settings['top_header'][:controller.settings['margin_top']])
+        else:
+            header = '\n'.join(controller.settings['top_header']) + (controller.settings['margin_top']-len(controller.settings['top_header']))*'\n'
+    else:
+        header = controller.settings['margin_top']*'\n'
+    return {'model_id': 'FramedList', 'first_row': header + '\n' + first_row + controller.settings['margin_right']*' ', 'second_row': second_row + controller.settings['margin_right']*' ', 'third_row': third_row + controller.settings['margin_right']*' ', 'divider': '┃', 'columns': columns}
